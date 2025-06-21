16ee4ea565e0bdd1f296a1b912cc8e6371edb92e
T1Ryan <ndabahariyeryanashera@donboscosdw.be>
2025-06-21 22:39:46 +0200

    web testings

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ const Home = () => {
             <h1>Welkom op mijn website</h1>
             <p>Dit is een website gemaakt door Ryan Ndabahariye. Deze dient als een portfolio site om overzicht van mijn project te krijgen.</p>
 
-            <p>Je kan naar het tab <strong>Portfolio</strong> om mijn projecten te bekijken.</p>
-            <p>Je kan naar het tab <strong>Contact</strong> om mij een mail te sturen</p>
+            <p>Je kan naar het tab Portfolio om mijn projecten te bekijken.</p>
+            <p>Je kan naar het tab Contact om mij een mail te sturen</p>
         </div>
     )
 };
